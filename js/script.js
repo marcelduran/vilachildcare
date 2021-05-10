4159e132bc7a4a7c40ade9a339375efcdb36779b
Marcel Duran <marcelduran@gmail.com>
2021-05-09 21:20:03 -0700

    first major update

--- js/script.js
+++ js/script.js
@@ -640,7 +640,7 @@ $document.ready(function () {
   if (plugins.rdGoogleMaps.length) {
     var i;
 
-    $.getScript("//maps.google.com/maps/api/js?key=AIzaSyAFeB0kVA6ouyJ_gEvFbMaefLy3cBCyRwo&sensor=false&libraries=geometry,places&v=3.7", function () {
+    $.getScript("//maps.google.com/maps/api/js?key=AIzaSyCCxr8XjqqPeeIRo84E8LRjKiq0L99sVcY&sensor=false&libraries=geometry,places&v=3.7", function () {
       var head = document.getElementsByTagName('head')[0],
         insertBefore = head.insertBefore;
 
